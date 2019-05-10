48180b7551ca7c0887dcbe99fc861ae0476531e1
vipul-vinsol <vipul@vinsol.com>
2019-05-10 12:09:13 +0530

    Completed version change

--- linux-and-shell-scripting-assignment/version-change.sh
+++ linux-and-shell-scripting-assignment/version-change.sh
@@ -8,7 +8,13 @@ if [[ -d $from ]]; then
 	all_files=( $(ls $from) )
 	number_of_files=${#all_files[@]}
 	if [[ -d $to ]]; then 
-		echo "TODO"
+		for (( i=0; i<$number_of_files; i++ ))
+		do 
+			file=${all_files[$i]}
+			cp $from/$file $to/$file
+			sed -i "1s/^/Version 2.0\n/" $to/$file
+		done
+		echo "DONE"
 	else
 		echo $to "is not a directory"
 	fi
