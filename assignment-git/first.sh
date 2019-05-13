69564c1c924eabc1453ff31e4e850e88f193bf7f
vipul-vinsol <vipul@vinsol.com>
2019-05-13 11:37:22 +0530

    <message>

--- assignment-git/first.sh
+++ assignment-git/first.sh
@@ -8,3 +8,9 @@ echo "this is new a.txt" > a.txt
 echo "this is added to b.txt" >> b.txt
 git diff
 git add .
+git checkout -- .
+echo "this is a new chaneg to b.txt" > b.txt
+git add .
+git reset 
+git add .
+git commit -m "<messsage>"
